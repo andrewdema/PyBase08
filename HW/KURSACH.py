97fe98a17dffdce2f8fc6e224ce782d8b9de6825
andrewdema <andrew.dema@ukr.net>
2018-12-18 13:00:08 +0200

    Kursach

---
+++ HW/KURSACH.py
@@ -0,0 +1,30 @@
+from calc import *
+
+calculator = {'+':add, '-':sub, '*':mul, '/':divi, '**':expon} #ссылка на функцию помещаем, не саму функцию!
+
+while True:
+    try:
+        x = float(input('Enter the first number: '))
+        oper = input('Enter the operation of the proposed: '+str(list(calculator.keys())))
+        y = float(input('Enter the second number: '))
+    except ValueError:
+        print('Please enter a number!')
+    else:
+        break
+
+result = None
+
+if oper in list(calculator.keys()):
+    result = calculator[oper](x,y) # calculator[oper] обращаемся к значению словаря по заданному ключу и вызываем ф-ию def
+
+# if result is not None:
+#     print(result)
+# else:
+#     print('None a number')
+
+try:
+    if result is not None:
+        print(result)
+except Exception as e:
+    print(e)
+
---
+++ HW/calc.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def divi(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError as e:
+        return e
+def expon(a, b):
+    return a ** b
+
